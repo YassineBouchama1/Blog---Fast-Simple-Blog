7b13a432902903fa738867f83cbc7f2a1abe155c
siskoweb <sisko4dev@gmail.com>
2024-10-01 19:48:31 +0100

    Start Challenge 10h

---
+++ src/siteConfig.js
@@ -0,0 +1,22 @@
+
+const siteConfig = {
+  title: 'Fast-Simple-Blog ',
+  author: 'YassineBouchama',
+  headerTitle: 'Next.js Blog',
+  description: 'A blog created with Next.js, Tailwind.css .',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://blog-nextjs-seo-tej3.vercel.app/', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'sisko4dev@gmail.com',
+  github: 'https://github.com/YassineBouchama1',
+  twitter: 'https://twitter.com/YassineWeb99',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com/YassineBouchama',
+  linkedin: 'https://www.linkedin.com/in/YassineBouchama/',
+  dribbble: 'https://www.dribbble.com',
+  locale: 'en-US',
+}
+
+module.exports = siteConfig
